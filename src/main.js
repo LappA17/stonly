b027ec882dc46532cbc54ae847cf27f889a58904
Ruslan <ruslan.postoyuk@gmail.com>
2022-11-22 11:17:03 +0100

    create orm configs

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const PORT = process.env.PORT || 8001;
-    const app = await NestFactory.create(AppModule);
-    await app.listen(PORT, () => console.log('Server started on the PORT ' + PORT));
+  const PORT = process.env.PORT || 8001;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT, () => console.log('Server started on the PORT ' + PORT));
 }
+
 bootstrap();
