3c60d25d0e054226c8142cbb99d8be90c24ada9b
Ruslan <ruslan.postoyuk@gmail.com>
2022-11-22 11:20:40 +0100

    create forFeature

--- src/issue/issue.module.ts
+++ src/issue/issue.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { IssueService } from './issue.service';
 import { IssueController } from '@app/issue/issue.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IssueEntity } from '@app/issue/models/issue.entity';
+import { UserEntity } from '@app/user/models/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([IssueEntity, UserEntity])],
   providers: [IssueService],
   controllers: [IssueController],
 })
