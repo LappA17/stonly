a6aa9ba7b7b65d51831a7156108c62caef195d8b
Ruslan <ruslan.postoyuk@gmail.com>
2022-11-22 11:18:40 +0100

    create for root

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { IssueController } from './issue/issue.controller';
 import { IssueModule } from './issue/issue.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from '@app/config/orm.config';
 
 @Module({
-  imports: [UserModule, IssueModule],
+  imports: [TypeOrmModule.forRoot(ormConfig), UserModule, IssueModule],
   controllers: [IssueController],
 })
 export class AppModule {}
